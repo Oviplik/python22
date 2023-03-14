23ea3da50518e34f45c4ad175f18c171d93f7afd
Oviplik <oviplik@mail.ru>
2023-03-14 20:35:50 +0500

    Revert "Revert "Revert "Update задание 7.py"""
    This reverts commit 3afa407d49dce0b7a92ac3493ecadab3001285be.

--- задание 7.py
+++ задание 7.py
@@ -1,7 +1,2 @@
 print('Hello world222222')
 print('Hello world5555555555555555')
-print('Hello world5555555555555555')
-print('Hello world5555555555555555')print('Hello world5555555555555555')
-print('Hello world5555555555555555')
-print('Hello world5555555555555555')
-print('Hello world5555555555555555')
