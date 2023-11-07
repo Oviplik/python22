d10f1f4d3cb14bf3ee7aeb9309f3f8d2c77f7d4e
Oviplik <127862344+Oviplik@users.noreply.github.com>
2023-11-07 20:09:09 +0500

    Delete Python Django_mysite/mysite directory

--- Python Django_mysite/mysite/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class UserData(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    age = models.PositiveIntegerField()
-    gender = models.CharField(max_length=10)
-    adres = models.CharField(max_length=150,default='none')
-    news = models.BooleanField(default=True)
