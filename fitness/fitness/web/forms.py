a62351df870ccac63d0fa29ebac5039c6d88f697
Oviplik <127862344+Oviplik@users.noreply.github.com>
2023-11-16 23:41:54 +0500

    Delete fitness directory

--- fitness/fitness/web/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import *
-
-class FeedbackForm(forms.Form):
-    name = forms.CharField( label = 'Введите имя:', widget = forms.TextInput(
-                attrs = {'placeholder':'Ваше имя'}))
-    phone = forms.CharField( label = 'Введите номер телефона:', widget = forms.TextInput(
-                attrs = {'placeholder':'Ваш номер телефона'}))
